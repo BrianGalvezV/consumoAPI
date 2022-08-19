711418a5fb1993f554a2b7a7334b402e71b916b9
Brian Galvez <kio_02@hotmail.com>
2022-08-19 04:59:18 -0500

    funcion delete

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?limit=&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?limit=&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46`
 
 const spanError = document.getElementById('error')
 
@@ -34,16 +35,24 @@ async function loadFavouriteMichis() {
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   }else{
+    const section = document.getElementById('favoriteMichis')
+    section.innerHTML = "";
+    const h2 = document.createElement('h2')
+    const h2Text = document.createTextNode('Michis Favoritos')
+    h2.appendChild(h2Text);
+    section.appendChild(h2)
+
+
     data.forEach(michi => {
-        const section = document.getElementById('favoriteMichis')
         const article = document.createElement('article');
         const img = document.createElement('img');
         const btn = document.createElement('button');
         const btnText = document.createTextNode('Sacar al michi de favoritos');
 
         btn.appendChild(btnText);
+        btn.onclick = () => deleteFavouriteMichi(michi.id);
         img.src = michi.image.url
-
+        img.width = 150;
         article.appendChild(img);
         article.appendChild(btn);
         section.appendChild(article);
@@ -70,7 +79,25 @@ async function saveFavouriteMichi(id) {
 
     if (res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+      }else{
+        console.log('Michi guardado en favorito')
+        loadFavouriteMichis();
       }
+      
+}
+
+async function deleteFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES_DELETE(id), {
+    method : 'DELETE',
+  });
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }else{
+    console.log('Michi Eliminado')
+    loadFavouriteMichis();
+  }
 }
 
 loadRandomMichis();
